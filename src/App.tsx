3b423e8389f189307179f166934d82390e609321
Nacho Martinez <developer.nacho@gmail.com>
2023-04-11 16:35:28 +0200

    Integration all components in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Card, Container, Spacer } from '@nextui-org/react';
+import { NextUIProvider } from '@nextui-org/react';
+
+import ToDoList from './components/ToDoList/ToDoList'
+import NavBar from './components/NavBar/NavBar'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const handleToDoCompleted = (todoId: string) => {
+    // MARK TODO COMPLETED
+    console.log('ToDo --->> Completed', todoId)
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <NextUIProvider>
+        <NavBar />
+        <Spacer y={4}/>
+        <Container fluid>
+          <Card >
+            <Card.Body>
+              <ToDoList markToDoAsCompleted={ handleToDoCompleted }/>
+            </Card.Body>
+          </Card>
+        </Container>
+      </NextUIProvider>
     </div>
   )
 }
